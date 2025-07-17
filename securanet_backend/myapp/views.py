6a3e4dd5dd7889ff7050c7826d1b0c56c0ae6a27
VICk-ario <njorogevictor310@gmail.com>
2025-07-17 12:50:53 +0300

    changed phishing model

--- securanet_backend/myapp/views.py
+++ securanet_backend/myapp/views.py
@@ -1,15 +1,29 @@
 #from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import api_view
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import clasify_message
+from transformers import pipeline
 
-@api_view(['POST'])
+classifier = pipeline("text-classification", model="cybersectony/phishing-email-detection-distilbert_v2.4.1")
+
+def classify_message(text):
+    result = classifier(text)[0]
+    label = result["label"]
+
+    # Map model output to your custom labels
+    verdict = "Suspicious" if label == "NEGATIVE" else "Not Suspicious"
+
+    return {
+        "verdict": verdict,
+        "confidence": round(result["score"], 3)
+    }
+
+@api_view(['GET','POST'])
 def check_phishing(request):
     message = request.data.get('message')
     if not message:
-        return Response({"error": "No mesage provided"}, status = 400)
+        return Response({"error": "No message provided"}, status = 400)
     
-    prediction = clasify_message(message)
+    prediction = classify_message(message)
     return Response(prediction)
